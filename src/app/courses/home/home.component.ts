b65f533e7a523bb1007442ac5f90015c743eeded
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-21 23:50:27 +0800

    update

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,80 +1,69 @@
-import {Component, OnInit} from '@angular/core';
-import {compareCourses, Course} from '../model/course';
-import {Observable} from "rxjs";
-import {defaultDialogConfig} from '../shared/default-dialog-config';
-import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import {map, shareReplay} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
-
-
+import { Component, OnInit } from "@angular/core";
+import { compareCourses, Course } from "../model/course";
+import { Observable } from "rxjs";
+import { defaultDialogConfig } from "../shared/default-dialog-config";
+import { EditCourseDialogComponent } from "../edit-course-dialog/edit-course-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { map, shareReplay } from "rxjs/operators";
+import { CoursesHttpService } from "../services/courses-http.service";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  promoTotal$: Observable<number>;
 
-    promoTotal$: Observable<number>;
+  loading$: Observable<boolean>;
 
-    loading$: Observable<boolean>;
+  beginnerCourses$: Observable<Course[]>;
 
-    beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
+  // 5 数据生命周期与home紧耦合，每次重建home component，都需要重新发送请求
 
-    advancedCourses$: Observable<Course[]>;
+  constructor(
+    private dialog: MatDialog,
+    private coursesHttpService: CoursesHttpService
+  ) {}
 
-
-    constructor(
-      private dialog: MatDialog,
-      private coursesHttpService: CoursesHttpService) {
-
-    }
-
-    ngOnInit() {
-      this.reload();
-    }
+  ngOnInit() {
+    this.reload();
+  }
 
   reload() {
+    const courses$ = this.coursesHttpService.findAllCourses().pipe(
+      map((courses) => courses.sort(compareCourses)),
+      shareReplay()
+    );
 
-    const courses$ = this.coursesHttpService.findAllCourses()
-      .pipe(
-        map(courses => courses.sort(compareCourses)),
-        shareReplay()
-      );
+    this.loading$ = courses$.pipe(map((courses) => !!courses));
 
-    this.loading$ = courses$.pipe(map(courses => !!courses));
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "BEGINNER")
+      )
+    );
 
-    this.beginnerCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'BEGINNER'))
-      );
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "ADVANCED")
+      )
+    );
 
-
-    this.advancedCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'ADVANCED'))
-      );
-
-    this.promoTotal$ = courses$
-        .pipe(
-            map(courses => courses.filter(course => course.promo).length)
-        );
-
+    this.promoTotal$ = courses$.pipe(
+      map((courses) => courses.filter((course) => course.promo).length)
+    );
   }
 
   onAddCourse() {
-
     const dialogConfig = defaultDialogConfig();
 
     dialogConfig.data = {
-      dialogTitle:"Create Course",
-      mode: 'create'
+      dialogTitle: "Create Course",
+      mode: "create",
     };
 
     this.dialog.open(EditCourseDialogComponent, dialogConfig);
-
   }
-
-
 }
